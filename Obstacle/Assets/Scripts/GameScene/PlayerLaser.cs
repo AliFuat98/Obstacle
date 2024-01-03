9bf21ead08705ce2684fc0d53dfd4abb4c1bca18
Ali Fuat <87227396+AliFuat98@users.noreply.github.com>
2024-01-03 23:11:47 +0300

    lazer beam

--- Obstacle/Assets/Scripts/GameScene/PlayerLaser.cs
+++ Obstacle/Assets/Scripts/GameScene/PlayerLaser.cs
@@ -2,13 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerLaser : MonoBehaviour
-{
+public class PlayerLaser : MonoBehaviour {
+  [SerializeField] GameObject laserVisual;
+
+  List<GameObject> createdVisuals;
+  float duration;
+
   void Start() {
+    createdVisuals = new List<GameObject>();
+
     GameInput.Instance.OnLaserAction += Instance_OnLaserAction;
+    duration = laserVisual.GetComponent<ParticleSystem>().main.duration;
   }
 
   private void Instance_OnLaserAction(object sender, System.EventArgs e) {
-    Debug.Log("laser Fired");
+    GameObject createdVisual = Instantiate(laserVisual, transform);
+
+    GameObject secondCreatedVisual = Instantiate(laserVisual, transform);
+    secondCreatedVisual.transform.rotation = Quaternion.Euler(0, -90, 0);
+
+    createdVisuals.Add(createdVisual);
+    createdVisuals.Add(secondCreatedVisual);
+
+    StartCoroutine(DestroyItseltf());
+  }
+
+  IEnumerator DestroyItseltf() {
+    yield return new WaitForSeconds(duration);
+    foreach (var visual in createdVisuals) {
+      Destroy(visual);
+    }
   }
 }
